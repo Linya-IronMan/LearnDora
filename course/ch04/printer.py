e324f6a4703c0c4c625dac3917ed1bbc0eb861c3
陈定涛 <chendingtao@reredata.com>
2026-09-09 09:00:51 +0800

    chore: 更新 ch04 数据流示例和忽略规则

--- course/ch04/printer.py
+++ course/ch04/printer.py
@@ -11,7 +11,11 @@ def main():
         if event["type"] == "INPUT":
             # event["value"] 是 Arrow 数组，用 .to_pylist() 变回普通 Python 列表好打印
             data = event["value"].to_pylist()
-            print(f"printer 收到：{data}", flush=True)
+            if event["id"] == "sender":
+                print(f"printer 收到来自 sender 的数据: {data}", flush=True)
+            elif event["id"] == "echo":
+                print(f"printer 收到来自 echo 的数据: {data}", flush=True)
+            # print(f"printer 收到：{data}", flush=True)
 
         elif event["type"] == "STOP":
             break
